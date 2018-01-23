2925085733efe30fd098f9838e57829d9ad5fec3
maheshhp <hpmahesh73@gmail.com>
2018-01-23 21:21:49 +0530

    Verified exercise five

--- ExerciseFive/call-apply.js
+++ ExerciseFive/call-apply.js
@@ -1,11 +1,11 @@
 let callAndApply = {
   caller(object, method, nameArg, ageArg, tShirtSizeArg) {
     // your code
-
+    method.call(object, nameArg, ageArg, tShirtSizeArg);
   },
   applier(object, method, argumentsArr) {
     // your code
-
+    method.apply(object, argumentsArr);
   },
 };
 module.exports = callAndApply;
